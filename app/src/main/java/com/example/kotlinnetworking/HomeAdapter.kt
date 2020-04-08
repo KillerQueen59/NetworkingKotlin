03cd768470daff5da9ddc15bfd79b1cbc92f7b8a
ramadhantiba <fauzanramadhan59@gmail.com>
2020-04-08 10:06:05 +0700

    perubahan

--- app/src/main/java/com/example/kotlinnetworking/HomeAdapter.kt
+++ app/src/main/java/com/example/kotlinnetworking/HomeAdapter.kt
@@ -1,13 +1,32 @@
 package com.example.kotlinnetworking
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Adapter
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.kotlinnetworking.HomeAdapter.HomeViewHolder
 import org.w3c.dom.Text
 
-class HomeAdapter (private val result: List<Result>) {
+class HomeAdapter (private val results: List<Result>) : RecyclerView.Adapter<HomeViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        return HomeViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_movie,parent,false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return results.count()
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        holder.bind(results[holder.adapterPosition])
+    }
 
 
     inner class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
@@ -17,9 +36,13 @@ class HomeAdapter (private val result: List<Result>) {
                 title.text = result.title
 
                 val desc = findViewById<TextView>(R.id.descMovie)
-
+                desc.text = result.desc
 
                 val image = findViewById<ImageView>(R.id.imageMovie)
+                Glide.with(context)
+                    .load("https://image.tmdb.org/t/p/original/" + result.poster )
+                    .apply(RequestOptions.overrideOf(200,250))
+                    .into(image)
 
             }
         }
