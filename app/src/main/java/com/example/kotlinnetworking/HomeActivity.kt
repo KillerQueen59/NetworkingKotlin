1a17865561b5fb9daccc419cc95469d85aa590eb
ramadhantiba <fauzanramadhan59@gmail.com>
2020-04-10 00:39:42 +0700

    Find Error

--- app/src/main/java/com/example/kotlinnetworking/HomeActivity.kt
+++ app/src/main/java/com/example/kotlinnetworking/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinnetworking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -18,19 +19,22 @@ class HomeActivity : AppCompatActivity() {
 
 
         val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        val dataSource = NetworkProvider.providesHttpAdapter().create(HomeDataSource::class.java)
+        val dataSource = NetworkProvider.providesHttpAdapter().create(HomeDataSource::class.java) //get data from api
         dataSource.discoverMovie().enqueue(object :  Callback<HomeResponse> {
             override fun onResponse(call: Call<HomeResponse>, response: Response<HomeResponse>) {
+                Log.d("ojan","berhasil")
                 progressBar.visibility = View.GONE
                 val result = response.body()?.result //
                 val recyclerView = findViewById<RecyclerView>(R.id.rv_movie)
                 recyclerView.addItemDecoration(DividerItemDecoration(this@HomeActivity,
                     DividerItemDecoration.VERTICAL))
+                recyclerView.adapter = HomeAdapter(result ?: emptyList())
 
             }
 
             override fun onFailure(call: Call<HomeResponse>, t: Throwable) {
-
+                Log.d("ojan","gagal")
+                Log.e(HomeActivity::class.java.simpleName,"${t.printStackTrace()}")
             }
 
 
