25a27053b859d2ee1943aadf19f989e2ab375ba7
ramadhantiba <fauzanramadhan59@gmail.com>
2020-04-13 17:14:31 +0700

    RxJava 1/2 way

--- app/src/main/java/com/example/kotlinnetworking/presentation/HomePresenter.kt
+++ app/src/main/java/com/example/kotlinnetworking/presentation/HomePresenter.kt
@@ -4,6 +4,9 @@ import com.example.kotlinnetworking.di.module.NetworkModule
 import com.example.kotlinnetworking.data.HomeDataSource
 import com.example.kotlinnetworking.data.HomeResponse
 import dagger.Module
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,20 +16,26 @@ class HomePresenter (
     private val view: HomeView,
     private val dataSource: HomeDataSource
 ){
+
+    private val disposable:CompositeDisposable = CompositeDisposable()
+
     fun discoverMovie(){
         view.onShowLoading()
-
-
-        dataSource.discoverMovie().enqueue(object : Callback<HomeResponse>{
-            override fun onResponse(call: Call<HomeResponse>, response: Response<HomeResponse>) {
+        dataSource.discoverMovie()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({response ->
                 view.onHideLoading()
-                view.onResponse(response.body()?.result ?: emptyList())
-            }
-            override fun onFailure(call: Call<HomeResponse>, t: Throwable) {
+                view.onResponse(response.result)
+            },{error ->
                 view.onHideLoading()
-                view.onFailure(t)
-            }
+                view.onFailure(error)
+            }).addTo(disposable)
 
-        })
+
+
+
+    }
+    fun onDetach(){
+        disposable.clear()
     }
 }
